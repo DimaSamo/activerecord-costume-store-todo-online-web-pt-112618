9dfdb88f49c401c172c84d59e946f541c0414b51
Dmitri Samotessov <dimasamo96@gmail.com>
2019-02-26 02:14:52 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,10 +7,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
       t.string :theme
       t.integer :price
       t.boolean :family_friendly
-      t.integer :num_of_employees
-
-      t.datetime :opening_time
-      t.datetime :closing_time
+      t.datetime :opening_date
+      t.datetime :closing_date
     end
   end
 end
